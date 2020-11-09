dc63eed72e11ce1522be06b3375321d98bb8bf20
unknown <marcos_henrique@outlook.com>
2020-11-09 17:18:38 -0300

    Prova01

---
+++ main.c
@@ -0,0 +1,56 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <conio.h>
+
+// Questao01 -  
+//  Faça um algoritmo que crie uma implemente uma agenda eletrônica. 
+//  Agenda deverá armazenar no máximo 100 elementos, contendo nome,
+//  idade, telefone e e-mail cada. O programa deve ter um menu com as seguintes opções 
+//  (UTLIZE FUNÇÕES):
+// 	Adicionar:
+//	Adiciona uma entrada na agenda com o nome, idade, telefone e e-mail de uma pessoa
+//	Pesquisar:
+//	Pesquisar pelo nome de um usuário. Exiba todas as informações de pessoas com o nome pesquisado
+//	Exibir:
+//	Exiba todas as informações de pessoas da agenda eletrônica. 
+//
+
+
+
+
+int main (void) //
+{
+ FILE *pont_arq;
+ char palavra[20];
+ 
+ pont_arqu = fopen
+ 	
+	char nome;
+	char idade;
+	char tel;
+	char email;
+	
+	printf("Digite o nome :");
+	printf("Digite a idade :");
+	printf("Digite o telefone:");
+	printf("Digite o email:");
+
+  scanf("%d", &V2);
+	
+  
+  resultado = ;
+  return(resultado); //retornando o valor para main
+}
+
+
+
+
+int main(int argc, char *argv[]) {
+	
+	return 0;
+}
+
+
+
+
+
